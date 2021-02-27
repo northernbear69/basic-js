51c80ddb732484d453301fa4c9ed747086f7213c
northernbear69 <northernbear69webdev@gmail.com>
2021-02-27 22:27:18 +0300

    feat: add func

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,6 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity !== 'string' || isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY) return false;
-  let result = (MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD);
-  return Math.ceil(result);
+  let result = Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / Math.log(2)) * HALF_LIFE_PERIOD);
+  return result;
 };
