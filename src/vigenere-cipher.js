3c14de739a90dddc5d467d7fc5ab33b48e4cf96b
northernbear69 <northernbear69webdev@gmail.com>
2021-02-28 02:08:14 +0300

    feat: add func

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,74 @@
 const CustomError = require("../extensions/custom-error");
-
+let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(machineType = true) {
+    this.machine = machineType
+  }
+
+
+  encrypt(str, key) {
+    if(!str || !key) throw new Error;
+    let result = [];
+    str = str.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    key = this.getFullKey(str, key);
+    let indexKeyLetter = 0;
+    str.forEach((el, index) => {
+      if(!abc.includes(el)) result.push(el);
+      else {
+        let letterPos = abc.indexOf(el);
+        let keyLetter = key[indexKeyLetter];
+        let strWithShift = this.shiftLine(keyLetter);
+        result.push(strWithShift[letterPos]);
+        indexKeyLetter += 1;
+      }
+    });
+
+    if(!this.machine) result = result.reverse();
+    return result.join('');
+  }
+
+  decrypt(str, key) {
+    if(!str || !key) throw new Error;
+    let result = [];
+    str = str.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    key = this.getFullKey(str, key);
+    let indexKeyLetter = 0;
+    str.forEach((el, index) => {
+      if(!abc.includes(el)) result.push(el);
+      else {
+        let keyLetter = key[indexKeyLetter];
+        let strWithShift = this.shiftLine(keyLetter);
+        let letterPos = strWithShift.indexOf(el);
+
+        result.push(abc[letterPos]);
+        indexKeyLetter += 1;
+      }
+    });
+
+    if(!this.machine) result = result.reverse();
+    return result.join('');
+  }
+
+  getFullKey(str, key) {
+    let strLength = str.length;
+
+    while(key.length < strLength) {
+      key = key.concat(key);
+    }
+
+    key = key.slice(0, strLength);
+    return key;
+  }
+
+  shiftLine(keyLetter) {
+    let keyLetterPos = abc.indexOf(keyLetter);
+    let result = abc.slice(keyLetterPos);
+    result = result.concat(abc);
+    result = result.slice(0, abc.length);
+    return result;
   }
 }
 
